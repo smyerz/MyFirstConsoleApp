fa8b0a620e4001b618aefd40d2d765a6641b99ec
Maugrins <shadowboxartsstore@gmail.com>
2025-04-08 09:34:13 -0700

    ConsoleApp

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+ 
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}           
+// See https://aka.ms/new-console-template for more information
